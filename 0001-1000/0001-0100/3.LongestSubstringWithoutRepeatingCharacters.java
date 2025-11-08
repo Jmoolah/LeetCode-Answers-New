d343b06e74f2633291321c298902bbeb258c38a2
Jmoolah <tiraquelindo@gmail.com>
2025-11-08 13:11:27 -0500

    Add solution for Length of Longest Substring problem

---
+++ 0001-1000/0001-0100/3.LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        char[] sArr = s.toCharArray();
+        int high = 0;
+    
+
+        int count = 0;
+        HashSet<Character> hs = new HashSet<>();
+        for(int i =0; i < s.length();i++){
+
+            hs.clear();
+
+            for(int j = i; j < s.length(); j++){
+                if(hs.contains(sArr[j])){
+                    break;
+                }
+                hs.add(sArr[j]);
+
+                
+            }
+            int check = hs.size();
+                if (check > count){
+                    count = check;
+                }
+            
+        }
+        return count;
+    }
+}
