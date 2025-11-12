828798f5ef139a025e333bb5b2ec83edb252b5ed
Jmoolah <tiraquelindo@gmail.com>
2025-11-11 20:35:03 -0500

    added problem 2

---
+++ 0001-1000/0001-0100/2_TwoSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer,Integer> hm = new HashMap<>();
+
+        int n = nums.length;
+
+        for(int i =0; i < n; i++){
+            Integer complement = target - nums[i];
+
+            if(hm.containsKey(complement)){
+                return new int[]{hm.get(complement), i};
+            } 
+            hm.put(nums[i], i);
+        }
+       
+        
+
+        return new int[]{};
+
+    }
+}
